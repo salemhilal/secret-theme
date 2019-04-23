4c0d10ee4bb95758aa904bee4dfc760abdb8abc1
Salem <salemhilal@gmail.com>
2019-04-22 23:14:55 -0400

    remove 'posted on/by' text

--- secret-theme/template-parts/content.php
+++ secret-theme/template-parts/content.php
@@ -17,16 +17,7 @@
 		else :
 			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
 		endif;
-
-		if ( 'post' === get_post_type() ) :
-			?>
-			<div class="entry-meta">
-				<?php
-				secretmagazine_posted_by();
-				secretmagazine_posted_on();
-				?>
-			</div><!-- .entry-meta -->
-		<?php endif; ?>
+		?>
 	</header><!-- .entry-header -->
 
 
