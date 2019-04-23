ce224f9fda1f2d747d58f23ff8ee4c380dd8cec9
Salem <salemhilal@gmail.com>
2019-04-22 22:53:54 -0400

    delete first theme cuz it'll live on in the great git history

--- secret-theme/first try/functions.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-function add_theme_scripts() {
-    wp_enqueue_style( "style", "https://unpkg.com/tachyons/css/tachyons.min.css" );
-}
-add_action( 'wp_enqueue_scripts', 'add_theme_scripts' );
